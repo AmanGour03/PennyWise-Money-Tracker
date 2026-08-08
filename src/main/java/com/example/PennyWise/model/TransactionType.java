67b82527a590e9d6f8df32a650cd20330b860fa8
AmanGour03 <compag2022@gmail.com>
2026-08-09 00:21:15 +0530

    Clean up project structure

--- src/main/java/com/example/PennyWise/model/TransactionType.java
+++
@@ -1,5 +0,0 @@
-package com.example.PennyWise.model;
-
-public enum TransactionType {
-    INCOME,EXPENSE;
-}
--- src/main/java/com/example/PennyWise/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.example.PennyWise.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-}
